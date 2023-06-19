8b987064942c5e94002b17ea2887c15095f6facf
Tr0ngTin <71576097+tr0ngtin11@users.noreply.github.com>
2023-06-20 01:47:50 +0700

    deploy

--- src/api/billApi.js
+++ src/api/billApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://127.0.0.1:8000/api";
+const baseUrl = "https://is207n12-be-production.up.railway.app//api";
 
 export const getAllBillUser = (id) => {
   return new Promise(async (resolve, reject) => {
